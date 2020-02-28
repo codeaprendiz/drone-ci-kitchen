54ad766d358270eb144e87f7212f316de7c2c30c
codeaprendiz <codeaprendiz@gmail.com>
2020-02-29 03:34:24 +0400

    Chore: update

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir ./.secrets
+mkdir ./.secrets 2>/dev/null
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase=$GPG_PASSPHRASE \
 --output ./.secrets/secrets.json secrets.json.gpg
