6abc0baba5e1f4ec3881f9b70d93652ac44464c5
codeaprendiz <codeaprendiz@gmail.com>
2020-03-06 17:54:24 +0400

    Chore: update

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 DIRECTORY"
   exit 1
@@ -10,9 +11,9 @@ nameOfDir=$1
 #read -p "Enter the folder where secrets need to be decrypted: " nameOfDir
 #nameOfDir=${nameOfDir:-task2-docker-compose}
 
-echo $nameOfDir
+echo "Directory being decrypted $nameOfDir"
 # Decrypt the file
-mkdir -p ./$nameOfDir/.secrets 2>/dev/null
+mkdir -p ./.secrets 2>/dev/null
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase=$GPG_PASSPHRASE \
---output ./$nameOfDir/.secrets/secrets.json ./$nameOfDir/secrets.json.gpg
+--output ./.secrets/secrets.json ./secrets.json.gpg
--- task2-docker-compose/run.sh
+++ task2-docker-compose/run.sh
@@ -5,8 +5,23 @@
 #
 ####################################
 
-../decrypt_secret.sh task2-docker-compose >/dev/null
+../decrypt_secret.sh task2-docker-compose
+
+cp -rfp docker-compose-model.yml docker-compose.yml
+
+systemOS=$(uname)
+if [ $systemOS = "Darwin" ]; then
+  sed -i '' "s/SUBSTITUTE_DRONE_RPC_SECRET/$(cat ./.secrets/secrets.json | jq -r .DRONE_RPC_SECRET)/g" docker-compose.yml
+  sed -i '' "s/SUBSTITUTE_DRONE_GITHUB_CLIENT_SECRET/$(cat ./.secrets/secrets.json | jq -r .DRONE_GITHUB_CLIENT_SECRET)/g" docker-compose.yml
+fi
+
+if [ $systemOS = "Linux" ]; then
+  sed -i "s/SUBSTITUTE_DRONE_RPC_SECRET/$(cat ./.secrets/secrets.json | jq -r .DRONE_RPC_SECRET)/g" docker-compose.yml
+  sed -i "s/SUBSTITUTE_DRONE_GITHUB_CLIENT_SECRET/$(cat ./.secrets/secrets.json | jq -r .DRONE_GITHUB_CLIENT_SECRET)/g" docker-compose.yml
+fi
+
 
 sudo docker-compose up -d
 
+rm -rf docker-compose.yml
 rm -rf ./secrets
